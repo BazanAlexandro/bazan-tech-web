7c9248c7d10bd0b18217313f6c7bfa372c005348
BazanAlexandro <alexander-bazan@outlook.com>
2022-10-27 13:39:42 +0300

    extracted animated title

--- components/common.tsx
+++ components/common.tsx
@@ -1,7 +1,35 @@
+import { useEffect } from 'react'
+import { useInView } from 'react-intersection-observer'
+import { animated, config, useSpring } from 'react-spring'
 import styled from 'styled-components'
 
-export const Title = styled.h2`
+export const TitleStyled = styled(animated.h2)`
 	text-align: center;
 	margin: 0em;
 	font-size: 2rem;
 `
+
+export const Title = (props: any) => {
+	const { ref, inView } = useInView({
+		threshold: 0.2
+	})
+
+	const [style, api] = useSpring({
+		from: {
+			opacity: 0,
+			y: -50
+		},
+		config: config.molasses
+	}, [])
+
+	useEffect(() => {
+		if (inView) {
+			api.start({
+				opacity: 1,
+				y: 0
+			})
+		}
+	}, [inView, api])
+
+	return <TitleStyled ref={ref} {...props} style={style} />
+}
