251efa55dfa341860e999cdd057bc34ce46ff902
BazanAlexandro <alexander-bazan@outlook.com>
2022-11-01 12:50:41 +0200

    elaboration on the perks section

--- components/home/sections/Perks/index.tsx
+++ components/home/sections/Perks/index.tsx
@@ -2,6 +2,7 @@ import { useEffect } from 'react'
 import { useInView } from 'react-intersection-observer'
 import { useSpring, config, useTrail } from 'react-spring'
 import { COLORS } from '../../../../constants/colors'
+import getImg from '../../../../utils/getImg'
 import * as Styles from './styles'
 
 /**
@@ -20,7 +21,7 @@ const PerksSection = () => {
 		config: config.molasses
 	}, [])
 
-	const [trail, trailApi] = useTrail(2, () => ({ opacity: 0, y: -50 }))
+	const [trail, trailApi] = useTrail(3, () => ({ opacity: 0, y: -50 }))
 
 	useEffect(() => {
 		if (inView) {
@@ -40,30 +41,40 @@ const PerksSection = () => {
 			data-bg={COLORS.light}
 			data-color={COLORS.lighter}
 		>
-			{trail[0] && (
-				<Styles.Bullet style={trail[0]}>
-					<Styles.BulletHeader>
-						Landing page development
-					</Styles.BulletHeader>
-					proceed the rise of your personal brand, more customized brand options, increase sales
-				</Styles.Bullet>
-			)}
+			<Styles.DotBackground />
 
 			<Styles.Bullet>
 				<Styles.HeaderCoutner ref={ref}>
 					{numberStyle.number.to(n => `${Math.round(n)}+`)}
 				</Styles.HeaderCoutner>
-				years of commercial experience
+				Years of commercial experience
 			</Styles.Bullet>
 
-			{trail[1] && (
-				<Styles.Bullet style={trail[1]}>
-					<Styles.BulletHeader>
-						Website support
-					</Styles.BulletHeader>
-					bug resolving of scripting issues, browser inconsistencies, styling errors
-				</Styles.Bullet>
-			)}
+			<Styles.PerkList>
+				{trail[0] && (
+					<Styles.Bullet style={trail[0]}>
+						<Styles.BulletHeader>
+							Landing page development
+						</Styles.BulletHeader>
+						Responsive Website and Feature development of Single Page Apps as well as Server-Side ones with React, Typescript, Next.js
+					</Styles.Bullet>
+				)}
+
+				{trail[1] && (
+					<Styles.Bullet style={trail[1]}>
+						<Styles.BulletHeader>
+							Website support
+						</Styles.BulletHeader>
+						Bug resolving of scripting issues, browser inconsistencies, styling errors
+					</Styles.Bullet>
+				)}
+
+				{trail[2] && (
+					<Styles.Bullet style={trail[2]}>
+						Performance optimizations, SEO, Google PageSpeed insights
+					</Styles.Bullet>
+				)}
+			</Styles.PerkList>
 		</Styles.Root>
 	)
 }
--- components/home/sections/Perks/styles.tsx
+++ components/home/sections/Perks/styles.tsx
@@ -4,6 +4,7 @@ import { COLORS } from '../../../../constants/colors'
 import { GradientTextBase } from '../../../common'
 
 export const Root = styled.div`
+	position: relative;
 	width: 100%;
 	min-height: 100vh;
 	background: ${COLORS.primary};
@@ -12,6 +13,19 @@ export const Root = styled.div`
 	padding: 1em 3em;
 	gap: 2em;
 	position: relative;
+	justify-content: center;
+`
+
+export const DotBackground = styled.div`
+	background-image: radial-gradient(currentColor .5px, transparent .5px);
+	background-size: calc(10 * 2.5px) calc(10 * 2.5px);
+	color: white;
+	position: absolute;
+	left: 0%;
+	top: 0;
+	width: 100%;
+	height: 100%;
+	opacity: 0.5;
 `
 
 export const Bullet = styled(animated.div)`
@@ -19,7 +33,7 @@ export const Bullet = styled(animated.div)`
 	flex-direction: column;
 	gap: 1em;
 	align-items: center;
-	text-align: center;
+	text-align: left;
 `
 
 export const BulletHeader = styled.h2`
@@ -27,7 +41,16 @@ export const BulletHeader = styled.h2`
 `
 
 export const HeaderCoutner = styled(GradientTextBase)`
-	width: 200px;
+	width: 300px;
 	margin: 0;
-	font-size: 11rem;
+	font-size: 15rem;
+	line-height: 1;
+	padding: 0;
+`
+
+export const PerkList = styled.div`
+	display: flex;
+	flex-direction: column;
+	gap: 1.5em;
+	max-width: 30em;
 `
